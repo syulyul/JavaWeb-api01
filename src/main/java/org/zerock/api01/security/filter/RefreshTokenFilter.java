c25c87dd69937102bf79204fbdf3ab83502e77af
syulyul <syul9800@gmail.com>
2023-02-03 17:55:13 +0900

    commit test

--- src/main/java/org/zerock/api01/security/filter/RefreshTokenFilter.java
+++ src/main/java/org/zerock/api01/security/filter/RefreshTokenFilter.java
@@ -1,8 +1,11 @@
 package org.zerock.api01.security.filter;
 
+import com.google.gson.Gson;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.zerock.api01.security.exception.RefreshTokenException;
 import org.zerock.api01.util.JWTUtil;
 
 import javax.servlet.FilterChain;
@@ -10,6 +13,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -33,6 +39,47 @@ public class RefreshTokenFilter extends OncePerRequestFilter {
         }
 
         log.info("Refresh Token Filter...run..............1");
+
+        //전송된 JSON에서 accessToken 과 refreshToken을 얻어온다.
+        Map<String, String> tokens = parseRequestJSON(request);
+
+        String accessToken = tokens.get("accessToken");
+        String refreshToken = tokens.get("refreshToken");
+
+        log.info("accessToken: " + accessToken);
+        log.info("refreshToken: " + refreshToken);
+
+        try{
+            checkAccessToken(accessToken);
+        }catch (RefreshTokenException refreshTokenException){
+            refreshTokenException.sendResponseError(response);
+            return; //더이상 실행할 필요 없음
+        }
+
+    }
+
+    private Map<String,String> parseRequestJSON(HttpServletRequest request) {
+
+        //JSON 데이터를 분석해서 mid, mpw 전달 값을 Map으로 처리
+        try(Reader reader = new InputStreamReader(request.getInputStream())){
+
+            Gson gson = new Gson();
+
+            return gson.fromJson(reader, Map.class);
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
+        return null;
+    }
+
+    private  void checkAccessToken(String accessToken) throws RefreshTokenException {
+        try{
+            jwtUtil.validateToken(accessToken);
+        }catch (ExpiredJwtException expiredJwtException){
+            log.info("Access Token has expired");
+        }catch (Exception exception){
+            throw new RefreshTokenException(RefreshTokenException.ErrorCase.NO_ACCESS);
+        }
     }
 
 }
