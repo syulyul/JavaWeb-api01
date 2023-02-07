d17733a080d8aad4702f1314f980aca14c6c29f5
syulyul <syul9800@gmail.com>
2023-02-07 14:04:52 +0900

    lunch commit

--- src/main/java/org/zerock/api01/controller/TodoController.java
+++ src/main/java/org/zerock/api01/controller/TodoController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.zerock.api01.dto.PageRequestDTO;
+import org.zerock.api01.dto.PageResponseDTO;
 import org.zerock.api01.dto.TodoDTO;
 import org.zerock.api01.service.TodoService;
 
@@ -33,6 +35,30 @@ public class TodoController {
         log.info("read tno: " + tno);
 
         return todoService.read(tno);
+    }
+
+    @GetMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
+    public PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO){
+
+        return todoService.list(pageRequestDTO);
+    }
+
+    @GetMapping(value = "/{tno}")
+    public Map<String, String> delete(@PathVariable Long tno){
+
+        todoService.remove(tno);
+        return Map.of("result", "success");
+    }
+
+    @PutMapping(value = "/{tno}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> modify(@PathVariable("tno") Long tno, @RequestBody TodoDTO todoDTO){
+
+        //잘못된 tno가 발생하지 못하도록
+        todoDTO.setTno(tno);
+
+        todoService.modify(todoDTO);
+
+        return Map.of("result", "success");
     }
 
 }
